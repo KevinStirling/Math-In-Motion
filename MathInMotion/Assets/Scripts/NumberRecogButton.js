e1ae4e63709ef76a22a3cf847415aec3fe544d76
Kevin Young <youngks93@gmail.com>
2014-11-09 17:21:57 -0500

    Created functional menu buttons

---
+++ MathInMotion/Assets/Scripts/NumberRecogButton.js
@@ -0,0 +1,19 @@
+﻿
+var Button: Sprite;
+var Button_Over: Sprite;
+var Button_Down: Sprite;
+
+function OnMouseEnter()
+{
+GetComponent(SpriteRenderer).sprite = Button_Over;
+}
+
+function OnMouseExit()
+{
+GetComponent(SpriteRenderer).sprite = Button;
+}
+
+function OnMouseDown()
+{
+Application.LoadLevel("NumberRecognition");
+}
