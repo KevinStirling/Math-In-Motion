0c84f3b6089ff657c9adc91de062242a6ffa3ead
Kevin Young <youngks93@gmail.com>
2014-12-16 22:32:27 -0500

    new buttons, new sounds, happy new year

--- MathInMotion/Assets/Scripts/NumberLearnGameController.cs
+++ MathInMotion/Assets/Scripts/NumberLearnGameController.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class NumberLearnGameController : MonoBehaviour {
 	public int score;
 	public GUIText scoreText;
+	public AudioClip voicePrompts ;
 
 	// Use this for initialization
 	void Start () {
 		score = 0;
 		updateScore ();
+		audio.clip = voicePrompts;
+		audio.Play();
+
 	}
 	
 	// Update is called once per frame
